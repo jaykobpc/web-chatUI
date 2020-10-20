c365b8e47a5adc007dcf1a46f29c78df68e959af
jaykobpc <watenga.jacob@gmail.com>
2020-10-21 00:33:24 +0300

    run&test

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: "#app",
+});
+
+var imgContextMenu = document.querySelectorAll("img");
+imgContextMenu.forEach(function (img) {
+  if (img) {
+    img.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+    });
+  }
+});
+
+;(function() {
+  var element = document.querySelector('.wxchatview__chat-group');
+  element.scrollTop = element.scrollHeight;
+})();
+
